a12041aa7d3fc0aedc2959994e44f62da9c3dc5e
Felipe Pacheco <felipepachecosa@gmail.com>
2021-05-13 17:08:48 +0100

    Update files to install and App for instal/Update

---
+++ chocoInstal.cmd
@@ -0,0 +1,25 @@
+REM Tools
+choco install winrar -y
+choco install spotify -y
+choco install ditto -y
+choco install notepadplusplus -y
+choco install googlechrome -y
+choco install adobereader -y
+choco install slack -y
+
+REM Developer
+choco install kafka-tool -y
+choco install vscode -y
+choco install visualstudio2019enterprise -y
+choco install postman -y
+choco install docker-desktop -y
+choco install fiddler -y
+choco install openvpn -y
+
+REM Developer DataBase
+choco install mongodb -y
+choco install robo3t -y
+choco install sql-server-2019 -y
+choco install sql-server-management-studio -y
+choco install apache-cassandra -y
+
